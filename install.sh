841af5ca25e08f2726236e343967c7c5b5ed6e6c
Oliver Gutiérrez <ogutsua@gmail.com>
2016-03-10 17:55:06 +0000

    Initial commit

---
+++ install.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+# Copyright (C) 2016 Oliver Gutiérrez
+#
+# This program is free software; you can redistribute it and/or
+# modify it under the terms of the GNU Lesser General Public
+# License as published by the Free Software Foundation; either
+# version 2.1 of the licence, or (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+# Lesser General Public License for more details.
+#
+# You should have received a copy of the GNU Lesser General Public
+# License along with this program; if not, see <http://www.gnu.org/licenses/>.
+#
+# Author: Oliver Gutiérrez <ogutsua@gmail.com>
+
+ansible-playbook ansible/playbooks/install.yml -i 'localhost,'
+
